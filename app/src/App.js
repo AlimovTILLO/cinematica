cc95b1991d5407cce087ae9c8ce71797eb7c8523
Alimov Tillo <alimovtillo@DESKTOP-E9IB5UA.localdomain>
2020-01-11 15:33:44 +0600

    v 0.0.2 with dinamic sorting and generated ratings

--- app/src/App.js
+++ app/src/App.js
@@ -14,8 +14,28 @@ class App extends Component {
     fetch(url)
       .then(resp => resp.json())
       .then(data => {
-        this.setState({ movies: data.sort((a, b) => b.rating - a.rating) });
+        this.setState({ movies: data });
       })
+  }
+
+  shouldComponentUpdate(nextState) {
+
+    function getRandomIndex(items) {
+      return Math.floor(Math.random() * items.length);
+    }
+
+    setTimeout(() => {
+      for (let i = 0; i < 3; i++) {
+        let removedItem = this.state.movies.splice(getRandomIndex(this.state.movies), 1);
+        removedItem[0].rating = (Math.random() * 10).toFixed(2)
+        this.state.movies.push(removedItem[0])
+        console.log(removedItem[0])
+      }
+      let newstate = this.state.movies;
+      this.setState({ movies: newstate })
+    }, 3000)
+
+    return this.state.movies !== nextState.movies;
   }
 
   render() {
@@ -23,20 +43,24 @@ class App extends Component {
     return (
       <div className="App">
         <table>
-          <tr>
-            <th>Названия</th>
-            <th>Рейтинг</th>
-            <th>Возрастное ограничение</th>
-          </tr>
-        {this.state.movies.map((post, index) => { 
-          return (
-            <tr key={index}>
-              <td>{post.title}</td>
-              <td>{post.rating}</td>
-              <td>{post.age}+</td>
+          <thead>
+            <tr>
+              <th>Названия</th>
+              <th>Рейтинг</th>
+              <th>Возрастное ограничение</th>
             </tr>
-          )
-        })}
+          </thead>
+          <tbody>
+            {this.state.movies.sort((a, b) => b.rating - a.rating).map((post, index) => {
+              return (
+                <tr key={index}>
+                  <td>{post.title}</td>
+                  <td>{post.rating}</td>
+                  <td>{post.age}+</td>
+                </tr>
+              )
+            })}
+          </tbody>
         </table>
       </div>
     )
