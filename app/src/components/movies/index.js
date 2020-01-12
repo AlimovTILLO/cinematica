0934dfc660b2c514be58bbf9e0cda346a40773a0
Alimov Tillo <alimovtillo@DESKTOP-E9IB5UA.localdomain>
2020-01-12 13:53:27 +0600

    v 0.0.5 with with fixing styles

--- app/src/components/movies/index.js
+++ app/src/components/movies/index.js
@@ -34,7 +34,7 @@ class Movies extends Component {
                 {this.state.movies.sort((a, b) => b.rating - a.rating).map((post, index) => {
                     return (
                         <div className="card" key={index}>
-                            <div className="card__title">{post.title}</div>
+                            <div className="card__title"><h4>{post.title}</h4></div>
                             <div className="card__rating">{post.rating}</div>
                             <div className="card__age">{post.age}+</div>
                             <div className="card__image"><img src={process.env.PUBLIC_URL + post.image} alt={post.title}></img></div>
